b6747b559316750c7661410c6bf4165fc26db902
nithyaleela35 <nithyaleela35@gmail.com>
2024-08-30 19:47:57 +0530

    Array Initialize

---
+++ ArrayInit.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ArrayInit {
+    public static void main(String[] args) {
+        // we are creatin g , declaring an d initializing 
+        // array in a sinlge line 
+        int[] a = {10,20,30,40};
+        System.out.println(Arrays.toString(a));
+    }
+}
